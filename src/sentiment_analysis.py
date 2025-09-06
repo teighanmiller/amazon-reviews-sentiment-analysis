ca9685c3232b2f0883ad3fa5a58f7cb67969b8af
Teighan Miller <teighanmiller@mac.lan>
2025-09-06 12:54:56 -0400

    added loading of vectorizer file on prediction

--- src/sentiment_analysis.py
+++ src/sentiment_analysis.py
@@ -5,26 +5,34 @@ from sklearn.metrics import accuracy_score, classification_report
 
 
 class SentimentAnalysis:
-    MODEL_PATH = "data/sentiment_model.pkl"
+    LOGISTIC_MODEL_PATH = "data/sentiment_model.pkl"
+    VECTORIZER_PATH = "data/vectorizer.pkl"
 
     def __init__(self):
         self.vectorizer = CountVectorizer()
         self.model = LogisticRegression()
 
     def load_model(self):
-        with open(self.MODEL_PATH, "r") as f:  # pylint: disable=w1514
+        with open(self.LOGISTIC_MODEL_PATH, "rb") as f:  # pylint: disable=w1514
             self.model = pickle.load(f)
 
+        with open(self.VECTORIZER_PATH, "rb") as f:  # pylint: disable=w1514
+            self.vectorizer = pickle.load(f)
+
     def write_model(self):
-        with open(self.MODEL_PATH, "w") as f:  # pylint: disable=w1514
-            pickle.dump(f)
+        with open(self.LOGISTIC_MODEL_PATH, "wb") as f:  # pylint: disable=w1514
+            pickle.dump(self.model, f)
+
+        with open(self.VECTORIZER_PATH, "wb") as f:  # pylint: disable=w1514
+            pickle.dump(self.vectorizer, f)
 
     def fit_model(self, training_vector, y_train: list):
         self.vectorizer.fit(training_vector)
         x_train = self.vectorizer.transform(training_vector)
         self.model.fit(X=x_train, y=y_train)
 
-    def predict(self, test_vector: str):
+    def predict(self, test_vector):
+        self.load_model()
         x_test = self.vectorizer.transform(test_vector)
         return self.model.predict(x_test)
 
